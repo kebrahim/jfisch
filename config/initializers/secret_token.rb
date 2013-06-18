2aeb8c7bce016cbf788fdd5ef8ef311ca39d0d5a
Kurt Ebrahim <kebrahim@gmail.com>
2013-06-18 18:06:34 -0400

    setup rails config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jfisch::Application.config.secret_token = 'fc942bb8fed810a98e07cbc0b8390dba65ab3a57e3fd5d6ad657802dbdf9eea4f3263225f0982bac15e15a6f3b3b65943cfd7e7a945572e533a9dcf413ea33fb'
