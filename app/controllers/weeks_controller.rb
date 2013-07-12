8dfb4a269e667ce6ed54a88549d162c0fd8a311b
Kurt Ebrahim <kurt.ebrahim@gmail.com>
2013-07-12 18:34:55 -0400

    admin can update week start times

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -5,7 +5,7 @@ class WeeksController < ApplicationController
   # GET /weeks.json
   def index
     @current_year = Date.today.year
-    @weeks = Week.where(year: @current_year)
+    @weeks = Week.where(year: @current_year).order(:number)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -23,6 +23,28 @@ class WeeksController < ApplicationController
     end
 
     # TODO update start times
-    redirect_to "/weeks"
+    updated_weeks = false
+    confirmation_message = ""
+    if !params["save"].nil?
+      Week.transaction do
+        begin
+          Week.where(year: Date.today.year).order(:number).each { |week|
+            week_start_time_string = params["weekstart" + week.number.to_s]
+            week_start_time = DateTime.strptime(week_start_time_string + " Atlantic Time (Canada)",
+                                                "%m/%d/%Y %I:%M %p %Z")
+            if week.start_time != week_start_time
+              week.update_attribute(:start_time, week_start_time)
+              updated_weeks = true
+            end
+          }
+          if updated_weeks
+            confirmation_message = "Successfully updated start times"
+          end
+        rescue Exception => e
+          confirmation_message = "Error: Invalid date formatting. Please try again."
+        end
+      end
+    end
+    redirect_to "/weeks", notice: confirmation_message
   end
 end
